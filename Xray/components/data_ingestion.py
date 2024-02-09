e4862c2d91953b8ec0f40a9f7238765c985df036
Ravi Kumar <ravikumar46931@gmail.com>
2024-02-09 13:37:19 +0530

    data ingestion, transformation, trainer included

--- Xray/components/data_ingestion.py
+++ Xray/components/data_ingestion.py
@@ -1,8 +1,8 @@
 import sys
 
 from Xray.cloud_storage.s3_operation import S3Operation
-#from Xray.constant.training_pipeline import *
-from Xray.entity.artifact_entity import DataIngestionArtifact
+from Xray.constant.training_pipeline import *
+from Xray.entity.artifacts_entity import DataIngestionArtifact
 from Xray.entity.config_entity import DataIngestionConfig
 from Xray.exception import XRayException
 from Xray.logger import logging
@@ -11,9 +11,10 @@ from Xray.logger import logging
 class DataIngestion:
     def __init__(self, data_ingestion_config: DataIngestionConfig):
         self.data_ingestion_config = data_ingestion_config
-        
-        self.s3=S3Operation()
-    def get_data_from_s3(self):
+
+        self.s3 = S3Operation()
+
+    def get_data_from_s3(self) -> None:
         try:
             logging.info("Entered the get_data_from_s3 method of Data ingestion class")
 
@@ -22,15 +23,19 @@ class DataIngestion:
                 bucket_name=self.data_ingestion_config.bucket_name,
                 bucket_folder_name=self.data_ingestion_config.s3_data_folder,
             )
+
             logging.info("Exited the get_data_from_s3 method of Data ingestion class")
 
         except Exception as e:
             raise XRayException(e, sys)
-    
-    def initiate_data_ingestion(self):
+        
+        
+
+    def initiate_data_ingestion(self) -> DataIngestionArtifact:
         logging.info(
             "Entered the initiate_data_ingestion method of Data ingestion class"
         )
+
         try:
             self.get_data_from_s3()
 
@@ -44,5 +49,6 @@ class DataIngestion:
             )
 
             return data_ingestion_artifact
+
         except Exception as e:
             raise XRayException(e, sys)
--- Xray/entity/artifact_entity.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from torch.utils.data.dataloader import Dataloader
-
-@dataclass
-class DataIngestionArtifact:
-    train_file_path: str
-    test_file_path: str
